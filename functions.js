c8212dacbd02a205d25c33b254807d3fbe83492d
Will Kotheimer <wkotheimer@gmail.com>
2020-07-20 20:56:51 -0500

    arrays

--- functions.js
+++ functions.js
@@ -1,22 +1,22 @@
-console.log("functions");
+// console.log("functions");
 
-const nuggetizer = animal => {
-    return `processed ${animal}`;
-}
+// const nuggetizer = animal => {
+//     return `processed ${animal}`;
+// }
 
-console.log(nuggetizer('pig'));
-console.log(nuggetizer('fish'));
-console.log(nuggetizer('chicken'));
+// console.log(nuggetizer('pig'));
+// console.log(nuggetizer('fish'));
+// console.log(nuggetizer('chicken'));
 
-const plusforty2 = number => {
-    return 42 + (number || 0);
-}
+// const plusforty2 = number => {
+//     return 42 + (number || 0);
+// }
 
-console.log(plusforty2(1));
-console.log(plusforty2())
+// console.log(plusforty2(1));
+// console.log(plusforty2())
 
-const birthyearthing = year => `In 2099 you will be ${2099 - year}`;
-console.log(birthyearthing(1976));
+// const birthyearthing = year => `In 2099 you will be ${2099 - year}`;
+// console.log(birthyearthing(1976));
 
 
 
